15cbd4a1d394ad3e7b53fee52346d0cde934f023
Phinam112 <126156940+Phinam112@users.noreply.github.com>
2024-03-11 19:29:22 +0700

    tangco

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Cô';
+const giftUrl = 'https://www.facebook.com/tam.lethithanh.96930/';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng Cô';
+const contentCard = 'Nhân Quốc tế Phụ nữ, với tất cả tấm chân tình mà con muốn gửi đến cô, chúc cho cô luôn vui vẻ tràn ngập niềm tin trong cuộc sống, ngày nào cũng luôn gặp may mắn và thành công trên con đường dạy học của mình, chăm bón những mầm non của mình thật tốt tươi.';
+
+
+const giftImage = 'co.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
